8628557a122311b434e9f08c674bf5c54e0ac8c5
Hamid <gmhamid6@gmail.com>
2024-12-11 17:16:18 +0330

    Category Controller written but Update in Category and County have problem and need ID for parameter

--- WorldWideNews.API/Controllers/CategoryController.cs
+++ WorldWideNews.API/Controllers/CategoryController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using WorldWideNews.API.Entities;
+using WorldWideNews.API.Repository.Interfaces;
+using WorldWideNews.Models.Dtos;
 
 namespace WorldWideNews.API.Controllers
 {
@@ -6,6 +10,117 @@ namespace WorldWideNews.API.Controllers
     [Route("api/[controller]")]
     public class CategoryController : Controller
     {
+        private readonly ICategoryRepository _repository;
+        private readonly IMapper _mapper;
+        public CategoryController(ICategoryRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
 
+        [HttpGet]
+        public async Task<ActionResult<ICollection<CategoryDto>>> GetCategories()
+        {
+            return Ok(await _repository.GetCategories());
+        }
+
+        [HttpGet("{CategoryName}", Name = "GetNewsCategory")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+
+        public async Task<ActionResult<CategoryDto>> GetNewsCategory(string CategoryName)
+        {
+            try
+            {
+                var Category = await _repository.GetNewsCategory(CategoryName);
+                if (Category == null)
+                {
+                    return NotFound();
+                }
+                return Ok(Category);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost(Name = "AddNewsCategory")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+
+        public async Task<ActionResult<bool>> AddNewsCategory([FromBody] CategoryDto NewCategory)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var CategoryMap = _mapper.Map<Category>(NewCategory);
+
+                    if (await _repository.AddNewCategory(CategoryMap))
+                    {
+                        return Ok("Successfully");
+                    }
+                    return BadRequest();
+                }
+                return BadRequest();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut(Name = "UpdateCategory")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+
+        public async Task<ActionResult<bool>> UpdateCategory([FromBody] CategoryDto UpdateCategory)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest();
+                }
+
+                if (await _repository.GetNewsCategory(UpdateCategory.Name) != null)
+                {
+                    var CategoryMap = _mapper.Map<Category>(UpdateCategory);
+                    
+                    if (await _repository.UpdateCategory(CategoryMap))
+                    {
+                        return Ok("Successfully");
+                    }
+                }
+                return NotFound();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("{CategoryID:int}",Name ="")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+
+        public async Task<ActionResult<bool>> DeleteCategory(int CategoryID)
+        {
+            try
+            {
+                if(await _repository.DeleteCategory(CategoryID))
+                {
+                    return Ok("Successfully");
+                }
+                return BadRequest();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
     }
 }
